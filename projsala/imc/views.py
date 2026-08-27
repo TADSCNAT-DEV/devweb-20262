6f3dae83d4504f9bae9580fd5d41fe6242e6801b
André Almeida <andre.almeida@escolar.ifrn.edu.br>
2026-08-27 17:29:14 +0000

    Criação da aplicação IMC.

--- projsala/imc/views.py
+++ projsala/imc/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Olá Mundo</h1>")
+def heloisa(request):
+    return HttpResponse("<h1>Olá Heloisa</h1>")
+def tabuada2(request):
+    n=2
+    texto=''
+    for numero in range(1,11):
+        resultado=n*numero
+        texto+=f'<h1>{n} x {numero}={resultado}</h1>'
+
+    return HttpResponse(texto)
